9990695474c0ffcc4dfa6c93eedbc87693291ad0
unknown <cleggct@gmail.com>
2019-03-05 13:01:31 -0800

    fixed a bug in operator==

--- rikis-salazar-pic10c-winter19-c0bfd29ea3cd/assignments/hw3/ring_queue.cpp
+++ rikis-salazar-pic10c-winter19-c0bfd29ea3cd/assignments/hw3/ring_queue.cpp
@@ -75,7 +75,7 @@ class RingQueue{
 
         bool operator==( const iterator& rhs ) const {
             // Replace the line(s) below with your code.
-            return (parent == rhs->parent) && (offset == rhs->offset); //the iterators are equal if they have the same parent and offset
+            return (parent == rhs.parent) && (offset == rhs.offset); //the iterators are equal if they have the same parent and offset
         }
 
         bool operator!=( const iterator& rhs ) const {
@@ -238,13 +238,13 @@ int main(){
     // Uncomment the block below only when you have a working implementation of
     // RingQueue<ItemType,int>::end().  If the implementation is not correct, it
     // might result in an infinite loop.
-    /**
+    
     std::cout << "Queue via iterators:\n";
     for ( auto it = rq.begin() ; it != rq.end() ; ++it ) {
         std::cout << "Value: " << *it << ", address: " << &(*it) << '\n';
     }
     std::cout << '\n';
-    */
+    
 
 
 
